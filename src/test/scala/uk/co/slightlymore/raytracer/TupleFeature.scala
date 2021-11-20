2c87a7f7be24ad0866673634f72e5c810b175591
Clinton Montague <iblamefish@gmail.com>
2021-11-20 18:19:07 +0000

    Add Vector test

--- src/test/scala/uk/co/slightlymore/raytracer/TupleFeature.scala
+++ src/test/scala/uk/co/slightlymore/raytracer/TupleFeature.scala
@@ -10,23 +10,39 @@ class TuplesFeature extends Feature {
       val w = 1.0
       val t = RTTuple(x, y, z, w)
 
-      Then("t.x = 4.3")
+      Then("The values of x, y, z, w are set as expected")
       assert(t.x == x)
-
-      And("t.y = 4.3")
       assert(t.y == y)
-
-      And("t.z = 4.3")
       assert(t.z == z)
-
-      And("t.w = 4.3")
       assert(t.w == w)
 
-      And("a should be a Point")
-      And("a should not be a Vector")
+      And("t should be a Point")
+      And("t should not be a Vector")
       t match {
         case RTPoint(_, _, _, _) => assert(true)
         case RTVector(_, _, _, _) => assert(false)
+      }
+    }
+
+    Scenario("A tuple with w=0.0 is a Vector") {
+      Given("t ← tuple(4.3, -4.2, 3.1, 0.0)")
+      val x = 4.3
+      val y = -4.2
+      val z = 3.1
+      val w = 0.0
+      val t = RTTuple(x, y, z, w)
+
+      Then("The values of x, y, z, w are set as expected")
+      assert(t.x == x)
+      assert(t.y == y)
+      assert(t.z == z)
+      assert(t.w == w)
+
+      And("t should not be a Point")
+      And("t should be a Vector")
+      t match {
+        case RTPoint(_, _, _, _) => assert(false)
+        case RTVector(_, _, _, _) => assert(true)
       }
     }
   }
